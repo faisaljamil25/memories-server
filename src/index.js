eab1eb774adece29834f79cb7abb2cb1bd74e7e1
Faisal Jamil <faisaljamilcs@gmail.com>
2021-03-18 02:07:08 +0530

    fix: minor bug

--- src/index.js
+++ src/index.js
@@ -9,11 +9,11 @@ dotenv.config();
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT || 8000;
 
