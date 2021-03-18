bfd3436790396ecb8da739d76169a1eb4fd2e9f1
Faisal Jamil <faisaljamilcs@gmail.com>
2021-03-18 21:23:25 +0530

    feat: update post route

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,3 +1,6 @@
+import express from "express";
+import mongoose from "mongoose";
+
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -18,5 +21,38 @@ export const createPost = async (req, res) => {
     res.status(201).json(newPost);
   } catch (error) {
     res.status(409).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with given id");
+  try {
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+      new: true,
+    });
+
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const getPost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with given id");
+  try {
+    const post = await PostMessage.findById(_id);
+    if (post === null) return res.status(404).send("id not found");
+    res.status(200).json(post);
+  } catch (error) {
+    console.log(error.message);
+    res.status(404).json({ message: error.message });
   }
 };
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,10 +1,17 @@
 import express from "express";
 
-import { getPosts, createPost } from "../controllers/posts.js";
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  getPost,
+} from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost);
+router.get("/:id", getPost);
 
 export default router;
